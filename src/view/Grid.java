51231faeaef07f639fb2cd9ec1287e27f3b27bcd
Victorel Ezequiel <ezequiel.victorel@gmail.com>
2015-09-21 00:44:01 -0300

    Galeria con scroll Se agrego la posibidad de scrollear, junto con la posibilidad de cambiar el tamaño de la misma.

--- src/view/Grid.java
+++ src/view/Grid.java
@@ -3,7 +3,7 @@ package view;
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
-import java.awt.GridLayout;
+import java.awt.FlowLayout;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.util.HashMap;
@@ -13,12 +13,14 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import javax.swing.JFrame;
-import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 
-import view.previewers.*;
+import view.previewers.Previewer;
 
 public class Grid {
 	private static HashMap<String, Class<?>> previewers;
+
 	static {
 		previewers = new HashMap<>();
 		ServiceLoader<Previewer> previewLoader = ServiceLoader.load(Previewer.class);
@@ -28,29 +30,37 @@ public class Grid {
 			}
 		}
 	}
+
 	private static Pattern ExtensionPattern = Pattern.compile("(\\..+)$");
-	
+
 	private static int GRID_WIDTH = 2;
 	private static int GRID_HEIGHT = 3;
 	private static int CELL_SIZE = 125;
-	
+
 	private JFrame frame;
+	private JPanel panel;
 	private LinkedList<Component> components = new LinkedList<>();
-	
+
 	private GridHandler handler;
+
 	public Grid(GridHandler h) {
 		handler = h;
 
 		frame = new JFrame("Galeria");
-		frame.setLayout(new GridLayout(GRID_HEIGHT, GRID_WIDTH));
+		/* No permite scroll */
+		//frame.setLayout(new GridLayout(GRID_HEIGHT, GRID_WIDTH));
+		/* De esta manera, se permite una galeria a pantalla completa */
+		panel = new JPanel(new WrapLayout(FlowLayout.LEFT));
 
-		frame.addWindowListener(new WindowAdapter(){
-				public void windowClosing(WindowEvent we) {
-					System.exit(0);
-				}
-			});
+	    frame.add(new JScrollPane(panel));
 		
-		frame.getContentPane().setBackground(new Color(0,0,0));
+		frame.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent we) {
+				System.exit(0);
+			}
+		});
+
+		frame.getContentPane().setBackground(new Color(0, 0, 0));
 	}
 
 	public void addElement(String element) {
@@ -64,19 +74,16 @@ public class Grid {
 				Previewer p = (Previewer) comp;
 				p.preview(element, handler, components.size());
 			}
-			frame.add(comp);
+			panel.add(comp);
 			components.add(comp);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
+
 	public void show() {
-		/* rellena la cuadricula con jlabels vacios para que el gridlayout no se deforme */
-		if (components.size() < GRID_HEIGHT * GRID_WIDTH) {
-			for (int i = 0; i < GRID_HEIGHT * GRID_WIDTH - components.size(); i++) {
-				frame.add(new JLabel());
-			}
-		}
+		frame.setSize((GRID_WIDTH + 1) * CELL_SIZE, (GRID_HEIGHT + 1) * CELL_SIZE);
+		/* Luego se puede jugar un poco con los maximos y minimos del frame */
 		frame.pack();
 		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
